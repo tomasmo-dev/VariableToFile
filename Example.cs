8d880c8d5f4d01e2a56de61d1b2220a1c91e3095
Tomáš Moravec <moravec.tomas@ssakhk.cz>
2022-01-06 15:56:40 +0100

    updated examples

--- Example.cs
+++ Example.cs
@@ -28,6 +28,24 @@ namespace VarToFile
             Console.WriteLine($"loaded value : {value}");
         }
 
+        static void GithubExample()
+        {
+            string save_this = "some string";
+
+            VarSave.Saver save = new VarSave.Saver(@"Your path where you want your file saved here");
+            int return_value = save.SaveToFile<string>(save_this);
+            // string variable saved
+
+            //To retrieve it from file use:
+            VarSave.Loader load = new VarSave.Loader(@"Your path to saved file here");
+            int return_value1 = load.LoadFromFile(); // variable loaded to memory
+            string value = load.GetVar<string>(); // gets the value from memory
+
+            //now value from file is saved into "value" variable
+
+            Console.WriteLine(value);
+        }
+
         static void Main()
         {
             SaveExample();
